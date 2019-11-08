3bdb7447c9288df8a89fadaa84017d62c80e56f6
Jakob Drusany <37750012+jakic12@users.noreply.github.com>
2019-11-08 13:23:27 +0100

    Renewed style, removed php

--- template/config.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" type="text/css" href="css/style1.css">
-<link href="https://fonts.googleapis.com/css?family=Cantarell" rel="stylesheet">
-
-<?php $domain = "http://109.182.0.197" ?>
